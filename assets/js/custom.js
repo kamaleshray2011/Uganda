53de0d51941240df5602e36ff440344075ae6bb8
kamaleshray2011 <46379436+kamaleshray2011@users.noreply.github.com>
2019-06-24 13:01:22 +0600

    Uganda safaries

---
+++ assets/js/custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $('.slider').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        dots: true,
+        smartSpeed: 500,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.brand').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        dots: false,
+        smartSpeed: 500,
+        nav: false,
+        responsive: {
+            0: {
+                items: 3
+            },
+            600: {
+                items: 4
+            },
+            1000: {
+                items: 6
+            }
+        }
+    })
+});
